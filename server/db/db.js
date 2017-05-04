111651c6cf2f532c6e4c6bc51613803c7ab538bb
tivrama <joelschoolnik@JoelSchoolniks-13MBP-0012.local>
2017-05-04 13:53:25 -0700

    DB-dev and DB-prod is connected

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 var mlab = require('../config/env.js').mlab;
 module.exports = {
   // INSERT YOUR DB URL PARAMETERS HERE
-  url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds127801.mlab.com:27801/translation'
+  // url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds127801.mlab.com:27801/translation'
   // url to testing DB.
-  // url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds127731.mlab.com:27731/translation-dev'
+  url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds127731.mlab.com:27731/translation-dev'
 };
