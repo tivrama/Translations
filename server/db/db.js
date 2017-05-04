4c87a8bb6f85d2f66c684dc95ce40688268b1316
tivrama <joelschoolnik@JoelSchoolniks-13MBP-0012.local>
2017-05-04 13:43:01 -0700

    DB

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,7 @@
 var mlab = require('../config/env.js').mlab;
-console.log('mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds127731.mlab.com:27731/translation-dev')
 module.exports = {
   // INSERT YOUR DB URL PARAMETERS HERE
-  // url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '[yoururl.mlab.com:17195/yourapp]'
+  url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds127801.mlab.com:27801/translation'
   // url to testing DB.
-  url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds127731.mlab.com:27731/translation-dev'
+  // url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds127731.mlab.com:27731/translation-dev'
 };
