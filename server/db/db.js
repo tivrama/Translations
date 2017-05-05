2070a418956aef1cd8421e6c9c30f5835e5ced50
tivrama <joelschoolnik@JoelSchoolniks-13MBP-0012.local>
2017-05-05 14:20:59 -0700

    Server edits for deploy

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,10 @@
 var mlab = require('../config/env.js').mlab;
 module.exports = {
   // INSERT YOUR DB URL PARAMETERS HERE
+
+  // url to prod DB (Uncomment for production/go-live)
   // url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds127801.mlab.com:27801/translation'
-  // url to testing DB.
+
+  // url to testing DB (Comment out when going live)
   url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds127731.mlab.com:27731/translation-dev'
 };
